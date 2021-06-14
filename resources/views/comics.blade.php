511c9160f56f190a0225740fda989bbe43d30948
AlexandreLemes91 <lemesalexandre91@gmail.com>
2021-06-14 16:36:02 +0200

    home, /comic, migration for comics, comic model

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+    <section class="container">
+        Comics
+    </section>    
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="p-2 text-center">
+    <h5>FOOTER HERE!</h5>
+</footer>
